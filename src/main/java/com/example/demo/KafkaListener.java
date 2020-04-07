09550223067ced61d8391007c714d311b2f7bd0e
SKCC <SKCC@SKCC18D00123.SKCC.NET>
2020-04-07 17:04:15 +0900

    first commit

---
+++ src/main/java/com/example/demo/KafkaListener.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaListener {
+    @StreamListener(Processor.INPUT)
+    public void onEventByString(@Payload String productChanged){
+        System.out.println(productChanged);
+    }
+}
---
+++ src/main/java/com/example/demo/Order.java
@@ -0,0 +1,63 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.util.MimeTypeUtils;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="ORDER_TABLE")
+public class Order {
+
+    @Id @GeneratedValue
+    Long id;
+    int qty;
+//    Long productId;
+    String productName;
+
+    @PostPersist
+    public void eventPublish(){
+        OrderPlaced orderPlaced = new OrderPlaced();
+        orderPlaced.setOrderId(this.getId());
+        orderPlaced.setProductName(this.getProductName());
+//        orderPlaced.setProductId(this.productId);
+        orderPlaced.setQty(this.qty);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = null;
+
+        try {
+            json = objectMapper.writeValueAsString(orderPlaced);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("JSON format exception", e);
+        }
+
+        Processor processor = DemoApplication.applicationContext.getBean(Processor.class);
+        MessageChannel outputChannel = processor.output();
+
+        outputChannel.send(MessageBuilder
+                .withPayload(json)
+                .setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON)
+                .build());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+}
